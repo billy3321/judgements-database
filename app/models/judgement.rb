d6bc94d6432606f87ddc3964dc8497f8e15dcfcf
billy3321 <billy3321@gmail.com>
2015-12-21 18:45:53 +0800

    add serilize attribute

--- app/models/judgement.rb
+++ app/models/judgement.rb
@@ -1,3 +1,12 @@
 class Judgement < ActiveRecord::Base
-  
+  serialize :judges, Array
+  serialize :prosecutors, Array
+  serialize :lawyers, Array
+  serialize :clerks, Array
+  serialize :plaintiffs, Array
+  serialize :defendants, Array
+  serialize :prosecutor_office, Array
+  serialize :creditors, Array
+  serialize :debtors, Array
+  serialize :judicial_associate_officer, Array
 end
