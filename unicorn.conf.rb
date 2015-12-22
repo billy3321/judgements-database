37b330c8aa57e4331a87f7c35bce7c07d469e593
billy3321 <billy3321@gmail.com>
2015-12-22 16:57:19 +0800

    update log name

--- unicorn.conf.rb
+++ unicorn.conf.rb
@@ -1,6 +1,6 @@
 working_directory ""
 worker_processes 4
-listen "/var/run/unicorn/jrf_website_unicorn.sock", :backlog => 64
-pid "/var/run/unicorn/jrf_website_unicorn.pid"
-stderr_path "/var/log/jrf_website_unicorn.stderr.log"
-stdout_path "/var/log/jrf_website_unicorn.stdout.log"
+listen "/var/run/unicorn/judgements-database.sock", :backlog => 64
+pid "/var/run/unicorn/judgements-database.pid"
+stderr_path "/var/log/judgements-database.stderr.log"
+stdout_path "/var/log/judgements-database.stdout.log"
